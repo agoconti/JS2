f712df6e5911e373a90b20b8992de3d76f6105e8
agoconti <contigianiagostina@gmail.com>
2023-09-12 17:56:24 -0300

    se agrega condicional pero no se ejecuta la función

--- js/main.js
+++ js/main.js
@@ -1,6 +1,72 @@
 alert("Te damos la bienvenida a la banca digital del BICLA");
 
-let montoANegociar = Number(
+let tipoCheque = prompt(
+  "Indicanos el tipo de valor a negociar: 1- Cheque físico o 2- e-cheq"
+);
+
+if (tipoCheque == 1) {
+  alert("La operación deberá realizarse en nuestra Sucursal");
+} else if (tipoCheque == 2) {
+  let montoANegociar = Number(
+    prompt(
+      "Por favor ingresá la sumatoria de los montos de los cheques que vas a negociar"
+    )
+  );
+
+  let plazo = Number(
+    prompt(
+      "Por favor indicanos el plazo máximo de vencimiento de los cheques: 45, 90, 180 ó 270 días"
+    )
+  );
+
+  function calculoPrestamo() {
+    switch (plazo) {
+      case 45:
+        alert(
+          "Acreditaremos $" +
+            (montoANegociar - montoANegociar * ((0.94 * plazo) / 365)).toFixed(
+              2
+            ) +
+            " en tu cuenta"
+        );
+        break;
+      case 90:
+        alert(
+          "Acreditaremos $" +
+            (montoANegociar - montoANegociar * ((0.98 * plazo) / 365)).toFixed(
+              2
+            ) +
+            " en tu cuenta"
+        );
+        break;
+      case 180:
+        alert(
+          "Acreditaremos $" +
+            (montoANegociar - montoANegociar * ((1.08 * plazo) / 365)).toFixed(
+              2
+            ) +
+            " en tu cuenta"
+        );
+        break;
+      case 270:
+        alert(
+          "Acreditaremos $" +
+            (montoANegociar - montoANegociar * ((1.15 * plazo) / 365)).toFixed(
+              2
+            ) +
+            " en tu cuenta"
+        );
+        break;
+      default:
+        alert("No se pudo realizar el cálculo correctamente");
+    }
+  }
+  calculoPrestamo();
+} else {
+  alert("Por favor ingresá 1 ó 2 para poder continuar");
+}
+
+/* let montoANegociar = Number(
   prompt(
     "Por favor ingresá la sumatoria de los montos de los cheques que vas a negociar"
   )
@@ -55,4 +121,4 @@ function calculoPrestamo() {
   }
 }
 
-calculoPrestamo();
+calculoPrestamo(); */
