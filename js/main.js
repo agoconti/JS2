547e82bcfed6d7cfdcd47a7d95c1819ef1903601
agoconti <contigianiagostina@gmail.com>
2023-09-12 18:49:54 -0300

    solucionado el problema de ejecución de la función

--- js/main.js
+++ js/main.js
@@ -18,13 +18,12 @@ if (tipoCheque == 1) {
       "Por favor indicanos el plazo máximo de vencimiento de los cheques: 45, 90, 180 ó 270 días"
     )
   );
-
   calculoPrestamo(plazo, montoANegociar);
 } else {
   alert("Por favor ingresá 1 ó 2 para poder continuar");
 }
 
-function calculoPrestamo() {
+function calculoPrestamo(plazo, montoANegociar) {
   switch (plazo) {
     case 45:
       alert(
@@ -66,60 +65,3 @@ function calculoPrestamo() {
       alert("No se pudo realizar el cálculo correctamente");
   }
 }
-
-/* let montoANegociar = Number(
-  prompt(
-    "Por favor ingresá la sumatoria de los montos de los cheques que vas a negociar"
-  )
-);
-
-let plazo = Number(
-  prompt(
-    "Por favor indicanos el plazo máximo de vencimiento de los cheques: 45, 90, 180 ó 270 días"
-  )
-);
-
-function calculoPrestamo() {
-  switch (plazo) {
-    case 45:
-      alert(
-        "Acreditaremos $" +
-          (montoANegociar - montoANegociar * ((0.94 * plazo) / 365)).toFixed(
-            2
-          ) +
-          " en tu cuenta"
-      );
-      break;
-    case 90:
-      alert(
-        "Acreditaremos $" +
-          (montoANegociar - montoANegociar * ((0.98 * plazo) / 365)).toFixed(
-            2
-          ) +
-          " en tu cuenta"
-      );
-      break;
-    case 180:
-      alert(
-        "Acreditaremos $" +
-          (montoANegociar - montoANegociar * ((1.08 * plazo) / 365)).toFixed(
-            2
-          ) +
-          " en tu cuenta"
-      );
-      break;
-    case 270:
-      alert(
-        "Acreditaremos $" +
-          (montoANegociar - montoANegociar * ((1.15 * plazo) / 365)).toFixed(
-            2
-          ) +
-          " en tu cuenta"
-      );
-      break;
-    default:
-      alert("No se pudo realizar el cálculo correctamente");
-  }
-}
-
-calculoPrestamo(); */
