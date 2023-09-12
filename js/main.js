6b84cb5fa50983d2ddd429a21b3809783b38361c
agoconti <contigianiagostina@gmail.com>
2023-09-11 22:59:29 -0300

    se mejora el uso de variable PLAZO

--- js/main.js
+++ js/main.js
@@ -15,28 +15,28 @@ function calculoPrestamo() {
     case 45:
       alert(
         "Acreditaremos $" +
-          parseInt(montoANegociar - montoANegociar * ((0.94 * 45) / 365)) +
+          parseInt(montoANegociar - montoANegociar * ((0.94 * plazo) / 365)) +
           " en tu cuenta"
       );
       break;
     case 90:
       alert(
         "Acreditaremos $" +
-          parseInt(montoANegociar - montoANegociar * ((0.98 * 90) / 365)) +
+          parseInt(montoANegociar - montoANegociar * ((0.98 * plazo) / 365)) +
           " en tu cuenta"
       );
       break;
     case 180:
       alert(
         "Acreditaremos $" +
-          parseInt(montoANegociar - montoANegociar * ((1.08 * 180) / 365)) +
+          parseInt(montoANegociar - montoANegociar * ((1.08 * plazo) / 365)) +
           " en tu cuenta"
       );
       break;
     case 270:
       alert(
         "Acreditaremos $" +
-          parseInt(montoANegociar - montoANegociar * ((1.15 * 270) / 365)) +
+          parseInt(montoANegociar - montoANegociar * ((1.15 * plazo) / 365)) +
           " en tu cuenta"
       );
       break;
