702f458fbeb6d8dcb8f53721050ef930c8b49df2
agoconti <127361326+agoconti@users.noreply.github.com>
2023-10-29 20:42:02 -0300

    se resuelve error de validacióne en form prestamos

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ fetch("./database/inversiones.json")
   .then(data => {
     setTimeout(() => {
       mostrarInversiones(data);
-    }, 3000);
+    }, 2000);
   })
   .catch(error => {
     console.error("Error al cargar los datos:", error);
@@ -75,25 +75,59 @@ let tasa;
 fetch("./database/inversiones.json")
   .then(response => response.json())
   .then(data => { 
-  tasa = data[0].tasa
-    })
+    tasa = data[0].tasa
+  })
   .catch(error => {
   console.error("Error al cargar los datos:", error);
-    })
+  })
 
 function res(monto, tasa, plazo) {
   let resultado = Number(monto) + (Number(monto) * ((tasa / 100) / 365) * plazo);
   resultadoPF.innerText = `Tu capital de $${monto} rendirá $${Number(resultado.toFixed(2))} al cabo de ${plazo} días`;
-  formInversiones.reset(); // Mover aquí para que se reinicie el formulario después de mostrar el resultado.
+  formInversiones.reset();
   return resultado;
 }
 
+// CONSTRUCTOR PRÉSTAMOS
+
+class ProductoFinanciero {
+  constructor(id, reglamentacion, nombre, plazo, tasa, usuario, destino, montoMinimo, montoMaximo, imagen) {
+    this.id = id;
+    this.reglamentacion = reglamentacion;
+    this.nombre = nombre;
+    this.plazo = plazo;
+    this.tasa = tasa;
+    this.usuario = usuario;
+    this.destino = destino;
+    this.montoMinimo = montoMinimo;
+    this.montoMaximo = montoMaximo;
+    this.imagen = imagen;
+  };
+};
+
 //RENDERIZADO DE PRÉSTAMOS
+
+const productosFinancieros = [];
+
+fetch("./database/prestamos.json")
+  .then(response => response.json())
+  .then(data => {
+      for (const productoData of data) {
+          const producto = new ProductoFinanciero(...Object.values(productoData));
+          productosFinancieros.push(producto);
+      }
+      setTimeout(() => {
+          mostrarPrestamos(data)
+      }, 3000);
+  })
+  .catch(error => {
+    console.error("Error al cargar los datos:", error);
+  });
 
 const grillaPrestamos = document.querySelector(".prestamos");
 
-function mostrarPrestamos(prestamos) {  
-  prestamos.forEach(prestamo => {
+function mostrarPrestamos(productosFinancieros) {  
+  productosFinancieros.forEach(prestamo => {
     let contenedor = document.createElement('div');
     contenedor.classList.add('col-4');
     contenedor.classList.add('prestamo');
@@ -104,26 +138,18 @@ function mostrarPrestamos(prestamos) {
   });
 }
 
-fetch("./database/prestamos.json")
-  .then(response => response.json())
-  .then(data => {
-    setTimeout(() => {
-      mostrarPrestamos(data);
-    }, 3000);
-  })
-  .catch(error => {
-    console.error("Error al cargar los datos:", error);
-  })
-
 //BUSQUEDA DE PRÉSTAMOS
+
+let resultadoPrestamo = document.querySelector(".resultado-prestamo");
 
 let formPrestamos = document.querySelector("#form-prestamos");
 let montoPrestamo = document.querySelector("#monto-prestamo");
 let plazoPrestamo = document.querySelector("#plazo-prestamo");
 
+
 function validarMontoPrestamo() {
   let montoValue = parseFloat(montoPrestamo.value);
-  if (isNaN(montoValue) || montoValue <= 50000) {
+  if (isNaN(montoValue) || montoValue < 50000) {
     Swal.fire('Ingresá un monto válido mayor que $50.000');
     return false;
   }
@@ -132,11 +158,26 @@ function validarMontoPrestamo() {
 
 function validarPlazoPrestamo() {
   let plazoValue = parseInt(plazoPrestamo.value);
-  if (isNaN(plazoValue) || plazoValue <= 0) {
+  if (isNaN(plazoValue) || plazoValue < 3) {
     Swal.fire('Ingresá un plazo válido mayor que 3');
     return false;
   }
   return true;
+}
+
+function calcularPrestamo() {
+  const montoSolicitado = parseFloat(montoPrestamo.value);
+  const plazoSolicitado = parseInt(plazoPrestamo.value);
+
+  for (const producto of productosFinancieros) {
+      const interes = calcularInteres(producto, plazoSolicitado, montoSolicitado);
+      if (interes !== null) {
+          resultadoPrestamo.innerText = `Para un monto de $${montoSolicitado} a ${plazoSolicitado} meses en ${producto.nombre}, el interés es de $${interes.toFixed(2)}`;
+          return;
+      }
+  }
+
+  Swal.fire('No se encontró un producto de préstamo adecuado.');
 }
 
 formPrestamos.addEventListener("submit", (e) => {
@@ -144,12 +185,22 @@ formPrestamos.addEventListener("submit", (e) => {
   resultadoPrestamo.innerText = "";
 
   if (validarMontoPrestamo() && validarPlazoPrestamo()) {
-    localStorage.setItem("monto-prestamo", JSON.stringify(montoPrestamo.value));
-    localStorage.setItem("plazo-prestamo", JSON.stringify(plazoPrestamo.value));
-    
-    let resultado = res(monto.value, tasa, plazoPrestamo.value);
+    let resultado = res(montoPrestamo.value, tasa, plazoPrestamo.value);
   }
 });
 
-//SIMULACIÓN DE PRÉSTAMOS
+function calcularInteres(producto, plazoSolicitado, montoSolicitado) {
+  if (
+      producto.plazo.includes(plazoSolicitado) &&
+      montoSolicitado >= producto.montoMinimo &&
+      (producto.montoMaximo === 'indistinto' || montoSolicitado <= producto.montoMaximo)
+  ) {
+      const tasaIndex = producto.plazo.indexOf(plazoSolicitado);
+      const tasa = producto.tasa[tasaIndex];
+      const interes = (montoSolicitado * tasa) / 100;
+      return interes;
+  } else {
+      return null;
+  }
+}
 
